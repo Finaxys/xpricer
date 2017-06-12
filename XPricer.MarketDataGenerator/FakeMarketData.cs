ea505d54e05c614deabac428b5d4a0e51bdba259
audricthevenet <athevenet@finaxys.com>
2017-06-12 17:37:22 +0200

    Fake data generator : added volatility

--- XPricer.MarketDataGenerator/FakeMarketData.cs
+++ XPricer.MarketDataGenerator/FakeMarketData.cs
@@ -13,24 +13,41 @@ namespace XPricer.MarketDataGenerator
         public static void CreateEquityQuotes()
         {
             var eq = GenerateEquityQuotes("MSF", new DateTime(2016, 06, 13), DateTime.Now, 44, 65, 44, 0.12M); // Microsoft
-            File.WriteAllText("MSF.csv", Serialize(eq));
+            File.WriteAllText("MSF-QUOTES.csv", Serialize(eq));
             eq = GetEquityQuotes("MSF");
+            var ev = GenerateEquityVolatilities(eq, 0.12M);
+            File.WriteAllText("MSF-VOLATILITIES.csv", Serialize(ev));
+            ev = GetEquityVolatilities("MSF");
 
             eq = GenerateEquityQuotes("FP", new DateTime(2016, 06, 13), DateTime.Now, 38, 48, 40, 0.1M); // Total
-            File.WriteAllText("FP.csv", Serialize(eq));
+            File.WriteAllText("FP-QUOTES.csv", Serialize(eq));
+            ev = GenerateEquityVolatilities(eq, 0.1M);
+            File.WriteAllText("FP-VOLATILITIES.csv", Serialize(ev));
 
             eq = GenerateEquityQuotes("AMZ", new DateTime(2016, 06, 13), DateTime.Now, 636, 900, 636, 0.05M); // Amazon
-            File.WriteAllText("AMZ.csv", Serialize(eq));
+            File.WriteAllText("AMZ-QUOTES.csv", Serialize(eq));
+            ev = GenerateEquityVolatilities(eq, 0.05M);
+            File.WriteAllText("AMZ-VOLATILITIES.csv", Serialize(ev));
         }
 
-        public static List<EquityQuote> GetEquityQuotes(string equityQuoteId)
+        public static List<EquityQuote> GetEquityQuotes(string equityId)
         {
-            if (String.IsNullOrWhiteSpace(equityQuoteId))
+            if (String.IsNullOrWhiteSpace(equityId))
                 throw new ArgumentException("Must not be null or whitespaces", "equityQuoteId");
-            equityQuoteId = equityQuoteId.ToUpperInvariant();
-            if (equityQuoteId != "MSF" && equityQuoteId != "FP" && equityQuoteId != "AMZ")
+            equityId = equityId.ToUpperInvariant();
+            if (equityId != "MSF" && equityId != "FP" && equityId != "AMZ")
                 throw new ArgumentException("Must be MSF, FP or AMZ", "equityQuoteId");
-            return Deserialize(File.ReadAllText(equityQuoteId + ".csv"));
+            return DeserializeQuotes(File.ReadAllText(equityId + "-QUOTES.csv"));
+        }
+
+        public static List<EquityVolatility> GetEquityVolatilities(string equityId)
+        {
+            if (String.IsNullOrWhiteSpace(equityId))
+                throw new ArgumentException("Must not be null or whitespaces", "equityId");
+            equityId = equityId.ToUpperInvariant();
+            if (equityId != "MSF" && equityId != "FP" && equityId != "AMZ")
+                throw new ArgumentException("Must be MSF, FP or AMZ", "equityId");
+            return DeserializeVolatilities(File.ReadAllText(equityId + "-VOLATILITIES.csv"));
         }
 
         // private
@@ -62,6 +79,18 @@ namespace XPricer.MarketDataGenerator
             return quotes;
         }
 
+        private static List<EquityVolatility> GenerateEquityVolatilities(List<EquityQuote> equityQuotes, decimal maxDailyVolatility)
+        {
+            var volatilities = new List<EquityVolatility>();
+            var pp = equityQuotes[0].Price * (1 - maxDailyVolatility / 2);
+            foreach (var eq in equityQuotes)
+            {
+                volatilities.Add(new EquityVolatility(eq.Key, eq.Date, Math.Abs(1 - (eq.Price / pp))));
+                pp = eq.Price;
+            }
+            return volatilities;
+        }
+
         private static string Serialize(List<EquityQuote> equityQuotes)
         {
             var sb = new StringBuilder();
@@ -70,7 +99,15 @@ namespace XPricer.MarketDataGenerator
             return sb.ToString();
         }
 
-        private static List<EquityQuote> Deserialize(string text)
+        private static string Serialize(List<EquityVolatility> equityVolatilities)
+        {
+            var sb = new StringBuilder();
+            foreach (var ev in equityVolatilities)
+                sb.AppendLine(ev.Key.Id + ";" + ev.Date + ";" + ev.Volatility);
+            return sb.ToString();
+        }
+
+        private static List<EquityQuote> DeserializeQuotes(string text)
         {
             StockKey key = null;
             var equityQuotes = new List<EquityQuote>();
@@ -86,6 +123,24 @@ namespace XPricer.MarketDataGenerator
                 }
             }
             return equityQuotes;
+        }
+
+        private static List<EquityVolatility> DeserializeVolatilities(string text)
+        {
+            StockKey key = null;
+            var equityVolatilities = new List<EquityVolatility>();
+            using (StringReader sr = new StringReader(text))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    var item = line.Split(new[] { ';' });
+                    if (key == null)
+                        key = new StockKey { Id = item[0] };
+                    equityVolatilities.Add(new EquityVolatility(key, DateTime.Parse(item[1]), Decimal.Parse(item[2])));
+                }
+            }
+            return equityVolatilities;
         }
     }
 }
---
+++ XPricer.Model/MarketData/EquityVolatility.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace XPricer.Model.MarketData
+{
+    public class EquityVolatility : MarketData<StockKey>
+    {
+        public DateTime Date      { get; }
+        public decimal Volatility { get; }
+
+        public EquityVolatility(StockKey key, DateTime date, decimal volatility)
+        {
+            Type       = "Equity Volatility";
+            Key        = key;
+            Date       = date;
+            Volatility = volatility;
+        }
+
+    }
+}
