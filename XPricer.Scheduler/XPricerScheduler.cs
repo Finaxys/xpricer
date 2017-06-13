257b8e7d3d204a68a17bc57501fe6acea2f91f2a
gbizet <guillbizet@gmail.com>
2017-06-13 14:16:03 +0200

    classe monitor added, todo the json export

--- XPricer.Scheduler/XPricerScheduler.cs
+++ XPricer.Scheduler/XPricerScheduler.cs
@@ -14,6 +14,7 @@ using NLog;
 using XPricer.Model;
 using XPricer.Model.Product;
 
+
 namespace XPricer.Scheduler
 {
 
@@ -86,7 +87,10 @@ namespace XPricer.Scheduler
             }
 
             return requestId;
+            
         }
+
+
 
         private string UploadRequestToBlob(ComputeRequest cr, String filename, CloudStorageAccount storageAccount, String containerName)
         {
