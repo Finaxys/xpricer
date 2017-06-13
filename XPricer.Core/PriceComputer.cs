73d1cf306574ec7119be142b6e37a24cdf72af06
audricthevenet <athevenet@finaxys.com>
2017-06-13 12:51:53 +0200

    PriceComputer cleanup

--- XPricer.Core/PriceComputer.cs
+++ XPricer.Core/PriceComputer.cs
@@ -1,40 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using NLog;
+﻿using NLog;
 using QLNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using XPricer.Model;
 using XPricer.Model.MarketData;
 using XPricer.Model.Product;
 using VanillaOption = XPricer.Model.Product.VanillaOption;
-using System.Linq;
 
 namespace XPricer.Core
 {
     internal class PriceComputer : IPriceComputer
     {
-        // Market Data
+        // Config
+        public PricingConfig Config  { get; }
+        public Product       Product { get; }
+
+         // Market Data
         public List<EquityQuote>      EquityQuotes       = new List<EquityQuote>();
         public List<EquityVolatility> EquityVolatilities = new List<EquityVolatility>();
 
-        // ReSharper disable once InconsistentNaming
-        private static readonly Logger logger = LogManager.GetCurrentClassLogger(typeof(PriceComputer));
+        private static readonly Logger _log = LogManager.GetCurrentClassLogger(typeof(PriceComputer));
 
-        public PricingConfig Config { get; }
-        public Product Product { get; }
-
+        // .ctor
         public PriceComputer(PricingConfig config, Product product)
         {
             Config  = config;
             Product = product;
         }
 
+        // compute the product
         public PricingResult Compute()
         {
             var vanillaOption = Product as VanillaOption;
             if (vanillaOption == null)
-            {
                 throw new NotSupportedException($"The product {Product.GetType()} is not supported!");
-            }
 
             var calendar = new TARGET();
             Settings.setEvaluationDate(Config.PricingDate);
@@ -49,7 +49,7 @@ namespace XPricer.Core
                     type = Option.Type.Call;
                     break;
                 default:
-                    throw new ArgumentOutOfRangeException($"The vanilla option type {vanillaOption.OptionType} is not supported!");
+                    throw new ArgumentException($"The vanilla option type {vanillaOption.OptionType} is not supported!");
             }
 
             var underlying = EquityQuotes.First(q => q.Key.Id == vanillaOption.Underlying
@@ -68,26 +68,24 @@ namespace XPricer.Core
             Date maturity = vanillaOption.Maturity;
             DayCounter dayCounter = new Actual365Fixed();
 
-            logger.Info("Option type = " + type);
-            logger.Info("Maturity = " + maturity);
-            logger.Info("Underlying price = " + underlying);
-            logger.Info("Strike = " + strike);
-            logger.Info("Risk-free interest rate = {0:0.000000%}", riskFreeRate);
-            logger.Info("Dividend yield = {0:0.000000%}", dividendYield);
-            logger.Info("Volatility = {0:0.000000%}", volatility);
+            _log.Info("Option type = " + type);
+            _log.Info("Maturity = " + maturity);
+            _log.Info("Underlying price = " + underlying);
+            _log.Info("Strike = " + strike);
+            _log.Info("Risk-free interest rate = {0:0.000000%}", riskFreeRate);
+            _log.Info("Dividend yield = {0:0.000000%}", dividendYield);
+            _log.Info("Volatility = {0:0.000000%}", volatility);
 
             Exercise europeanExercise = new EuropeanExercise(maturity);
 
             var underlyingH = new Handle<Quote>(new SimpleQuote(Convert.ToDouble(underlying)));
 
             // bootstrap the yield/dividend/vol curves
-            var flatTermStructure = new Handle<YieldTermStructure>(new FlatForward(Config.PricingDate, riskFreeRate, dayCounter));
-            // ReSharper disable once InconsistentNaming
-            var flatDividendTS = new Handle<YieldTermStructure>(new FlatForward(Config.PricingDate, dividendYield, dayCounter));
-            // ReSharper disable once InconsistentNaming
-            var flatVolTS = new Handle<BlackVolTermStructure>(new BlackConstantVol(Config.PricingDate, calendar, Convert.ToDouble(volatility), dayCounter));
+            var flatTermStructure    = new Handle<YieldTermStructure>(new FlatForward(Config.PricingDate, riskFreeRate, dayCounter));
+            var flatDividendTs       = new Handle<YieldTermStructure>(new FlatForward(Config.PricingDate, dividendYield, dayCounter));
+            var flatVolTs            = new Handle<BlackVolTermStructure>(new BlackConstantVol(Config.PricingDate, calendar, Convert.ToDouble(volatility), dayCounter));
             StrikedTypePayoff payoff = new PlainVanillaPayoff(type, strike);
-            var bsmProcess = new BlackScholesMertonProcess(underlyingH, flatDividendTS, flatTermStructure, flatVolTS);
+            var bsmProcess           = new BlackScholesMertonProcess(underlyingH, flatDividendTs, flatTermStructure, flatVolTs);
 
             // options
             var option = new QLNet.VanillaOption(payoff, europeanExercise);
@@ -96,20 +94,17 @@ namespace XPricer.Core
             // Monte Carlo Method: MC (crude)
             const int timeSteps = 1;
             var mcSeed = Config.NumberOfPaths;
-            var mcengine = new MakeMCEuropeanEngine<PseudoRandom>(bsmProcess)
-               .withSteps(timeSteps)
-               .withAbsoluteTolerance(0.02)
-               .withSeed(mcSeed)
-               .value();
-
+            var mcengine = new MakeMCEuropeanEngine<PseudoRandom>(bsmProcess).withSteps(timeSteps)
+                                                                             .withAbsoluteTolerance(0.02)
+                                                                             .withSeed(mcSeed)
+                                                                             .value();
             option.setPricingEngine(mcengine);
 
             var result = new PricingResult(option.NPV());
 
-            logger.Info($"NPV: {result.Value}");
+            _log.Info($"NPV: {result.Value}");
 
             return result;
         }
-
     }
 }
